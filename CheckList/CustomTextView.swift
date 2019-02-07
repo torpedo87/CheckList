44ce9e01d06def72f2cc3f729bd024ddb074140f
torpedo87 <bestofhand@naver.com>
2019-02-07 11:39:22 +0900

    add textview

---
+++ CheckList/CustomTextView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextView.swift
+//  CheckList
+//
+//  Created by junwoo on 07/02/2019.
+//  Copyright © 2019 samchon. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextView: UITextView {
+  
+  var checked: String = "v"
+  var unChecked: String = "ㅁ"
+  var listMode: Bool = false
+  
+  
+}
--- CheckList/ViewController.swift
+++ CheckList/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  private lazy var customTextView: CustomTextView = {
+    let textView = CustomTextView()
+    textView.translatesAutoresizingMaskIntoConstraints = false
+    textView.layer.borderWidth = 1
+    textView.delegate = self
+    return textView
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    view.backgroundColor = .white
+    view.addSubview(customTextView)
     
+    customTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+    customTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50).isActive = true
+    customTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50).isActive = true
+    customTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100).isActive = true
   }
-
-
 }
 
+extension ViewController: UITextViewDelegate {
+  
+}
