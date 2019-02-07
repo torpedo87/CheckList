21eeefd4ab1b8f839038eb0b6f4aca5481d2fea0
torpedo87 <bestofhand@naver.com>
2019-02-07 11:17:15 +0900

    init

---
+++ CheckList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckList
+//
+//  Created by junwoo on 07/02/2019.
+//  Copyright © 2019 samchon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+
+}
+
